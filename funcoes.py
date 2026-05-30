0c76b75bc7213eef99b3e61852266daadef643a8
c.helena <cindyjelinsky@yahoo.com.br>
2026-05-29 22:03:54 -0300

    concluido main funcoes

--- funcoes.py
+++ funcoes.py
@@ -1,6 +1,8 @@
 import csv
+import re
+from datetime import datetime
 
-
+#SPRINT 1 LEITURA CSV
 
 def ler_csv(caminho_arquivo):
 
@@ -15,3 +17,82 @@ def ler_csv(caminho_arquivo):
         return list(leitor)
 
 
+#SPRINT 2 LIMPEZA DE TEXTO
+
+def tratar_categoria(nome_categoria):
+ 
+
+ if not nome_categoria or nome_categoria.strip() == "":
+    return "sem categoria"
+ 
+ nome_categoria = nome_categoria.lower().strip()
+
+
+ nome_categoria = re.sub(
+
+ r"[^a-zA-ZÀ-ÿ0-9\s]",
+
+ "",
+
+ nome_categoria
+)
+
+ return nome_categoria
+
+#SPRINT 3 LIMPEZA DE NULOS
+
+def tratar_dim_produto(produto):
+    
+    dimensoes = [
+        "product_weight_g", 
+        "product_length_cm", 
+        "product_height_cm", 
+        "product_width_cm"
+    ]
+
+    campos_corrigidos = 0;     
+    
+        
+    for dim in dimensoes:
+        valor = produto.get(dim)
+        
+        
+        if not valor or valor.strip() == "":
+            produto[dim] = 0  
+            campos_corrigidos += 1
+            
+    return campos_corrigidos
+
+#SPRINT 4 REGRAS de NEGOCIO e DATAS
+
+def analise_hipotese_entrega(pedido):
+   
+    data_entrega = pedido.get("order_delivered_customer_date")
+    status = pedido.get("order_status")
+    
+    
+    if data_entrega and data_entrega.strip() != "":
+        return "entregue"
+        
+   
+    else:
+        return status
+    
+
+
+
+def formatar_data_br(string_data):
+   
+    if not string_data or string_data.strip() == "":
+        return string_data
+        
+    try:
+        
+        data_objeto = datetime.strptime(string_data.strip(), "%Y-%m-%d %H:%M:%S")
+        
+        data_br = data_objeto.strftime("%d/%m/%Y")
+        
+        return data_br
+        
+    except ValueError:
+        return string_data
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from funcoes import (
     ler_csv,
+    tratar_categoria,
+    tratar_dim_produto,
+    analise_hipotese_entrega,
+    formatar_data_br
     )
 
 
@@ -17,21 +21,78 @@ def main():
     nulos_categoria = 0
     nulos_dimensoes = 0
     total_cancelados = 0
-
+    teste_nulas = 0
     total_nulos_corrigidos = 0
-    hipotese_confirmada = True
+
+    nulos_em_transito = 0
+    nulos_processando = 0
+    nulos_outros = 0
+   
 
     # ----PROCESSAMENTO DATASET PRODUTOS ----
 
-    dados_pedidos = ler_csv(caminho_produtos)
+    dados_produtos = ler_csv(caminho_produtos)
 
-    print(dados_pedidos[:10])
-
-    
-
+    linhas_produtos = len(dados_produtos)
 
     
+    #print(dados_produtos[:10])
 
+    print("======TRATANDO DATASET PRODUTOS======")
 
+    for produto in dados_produtos:
+       
+     
+
+     categoria_original = produto.get("product_category_name")
+     categoria_limpa =  tratar_categoria(categoria_original)
+
+     produto["product_category_name"] = categoria_limpa
+
+     if categoria_limpa == "sem categoria":
+        nulos_categoria +=1
+        
+   
+     qnt_dimendoes_corrigidas = tratar_dim_produto(produto)
+     nulos_dimensoes += qnt_dimendoes_corrigidas
+    
+
+    # ----PROCESSAMENTO DATASET PEDIDOS ----
+
+    dados_ordens = ler_csv(caminho_pedidos)
+    linhas_pedidos = len(dados_ordens)
+
+    print("======TRATANDO DATASET PEDIDOS======")
+
+    for pedido in dados_ordens:
+        
+       
+        resultado = analise_hipotese_entrega(pedido)
+        
+        
+        if resultado == "entregue":
+            pass 
+            
+        elif resultado == "canceled":
+           total_cancelados += 1
+            
+        elif resultado == "shipped":
+           
+            nulos_em_transito += 1  
+
+        elif resultado == "processing" or resultado == "approved":
+            
+            nulos_processando += 1  
+            
+        else:
+            nulos_outros += 1
+
+        data_original = pedido.get("order_approved_at")
+        pedido["order_approved_at"] = formatar_data_br(data_original)
+    
+
+    
+    
+   
 if __name__ == "__main__":
     main()
